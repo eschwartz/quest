9a8d85435716036d48d0073753689fa66d1589ff
Edan Schwartz <edanschwartz@gmail.com>
2022-07-31 16:17:28 -0500

    Add health check endpoint

--- src/000.js
+++ src/000.js
@@ -47,4 +47,13 @@ exec('bin/006 ' + JSON.stringify(req.headers), (err, stdout, stderr) => {
 });
 });
 
+// Health check endpoint
+// Will be used by ECS to know that this task is health
+// ECS will restart the service, if this returns a non-2xx status
+// To check this endpoint, run:
+//  curl --fail http://localhost/health
+app.get('/health', function (req, res) {
+  res.sendStatus(200);
+});
+
 app.listen(port, () => console.log(`Rearc quest listening on port ${port}!`))
